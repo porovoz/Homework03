ce6e14d5213c926c54a55b8454f949c170386ebc
porovoz <falkon1@list.ru>
2022-10-19 20:56:22 +0300

    homework 03 started

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+       //video lesson 1
+        short bananas = 200;
+        System.out.println("Бананов " + bananas + " кг");
+
+        float sugar = 3;
+        float onePortion = sugar / 4;
+        System.out.println("Одна порция сахара весит " + onePortion + " кг");
+
+        byte a = 1;
+        short b = 1;
+        int c = 1;
+        int d = a + b + c;
+        System.out.println(d);
+
+        float g = a + 1f;
+        System.out.println(g);
+
+        //homework 03 started
 
     }
 }
